23f10353ace8990b266d1b7da5fea8b3013f2e14
Hoang Quan <hoangquan1097@gmail.com>
2020-09-24 12:00:25 +0700

    relationship fast api

--- app/serializers/developer_serializer.rb
+++ app/serializers/developer_serializer.rb
@@ -12,12 +12,10 @@
 # end
 class DeveloperSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :languages, :developer_languages
-  def languages
-    languages = object.languages.pluck(:language_name).join(", ")
-  end
 
-  def developer_languages
-    developer_languages = object.programming_languages.pluck(:name).join(", ")
-  end
+  has_many :languages, through: :developer_languages
+  has_many :programming_languages, through: :developer_programming_languages
+
+  attributes :id, :email
+  
 end
