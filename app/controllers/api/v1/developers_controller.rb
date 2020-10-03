6db91c2d4bea97607d127472b978c63e38c914cb
Hoang Quan <hoangquan1097@gmail.com>
2020-10-03 15:10:50 +0700

    create Api object with association and transaction

--- app/controllers/api/v1/developers_controller.rb
+++ app/controllers/api/v1/developers_controller.rb
@@ -31,28 +31,47 @@ module Api
             end
 
             def create
-                # byebug
+                arr_id_languages = developer_languages_params.dig(:relationships, :languages, :data)
+                arr_id_programming_languages = developer_programming_languages_params.dig(:relationships, :programming_languages, :data)
                 developer = Developer.new(developer_params)
-                if developer.save
-                    render json: {
+                ActiveRecord::Base.transaction do
+                    developer.save
+                    
+                    arr_id_languages.each do |id|    
+                        # byebug    
+                        language = Language.find(id.values[0].to_i)
+                        developer.languages << language
+                        # raise ActiveRecord::RecordInvalid if language.nil?
+                    end
+
+                    arr_id_programming_languages.each do |id|
+                        programming_language = ProgrammingLanguage.find(id.values[0].to_i)
+                        developer.programming_languages << programming_language
+                        # raise ActiveRecord::RecordInvalid if programming_language.nil?
+                    end
+                end
+                render json: {
                         success: true,
                         data: DeveloperSerializer.new(developer)
                     }
-                else
-                
-                    render_error(404, "title", "e") and return
-                end
-            end
-
-
-            private
-            def developer_params
-                # byebug
-                # params.require(:developer).permit(:data => [:attributes])
-                params.require(:data).permit(:attributes => [:email])
+            rescue ActiveRecord::RecordInvalid => e
+            render_error(404, "NotFound", e) and return
             end
 
             
+
+            private
+            def developer_params
+                params.require(:data).permit(:attributes => [:email])
+            end
+
+            def developer_languages_params
+                params.require(:data).permit(:relationships => [:languages => [:data => [:id]]])
+            end
+            
+            def developer_programming_languages_params
+                params.require(:data).permit(:relationships => [:programming_languages => [:data => [:id]]])
+            end
 
         end
     end
